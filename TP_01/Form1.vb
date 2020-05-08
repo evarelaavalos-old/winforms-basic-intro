e60a0e750efac2bad1dc1dfe16a537e017ee767b
Eric Varela Avalos <ericnvarelaavalos@gmail.com>
2020-05-08 11:36:22 -0300

    adding the lasts error checks

--- TP_01/Form1.vb
+++ TP_01/Form1.vb
@@ -2,6 +2,9 @@
 
     Private Enum EstadoFormulario
         Estado_Invalido
+        No_Selecciono_Equipo_Local
+        No_Selecciono_Equipo_Visitante
+        Equipos_Duplicados
         Goles_Local_Es_Vacio
         Goles_Local_No_Numerico
         Goles_Local_Negativo
@@ -9,13 +12,12 @@
         Goles_Visitante_No_Numerico
         Goles_Visitante_Negativo
         No_Selecciono_Finalizacion
-        Equipos_Duplicados
         Equipos_Enfrentados_Previamente
         Correcto
     End Enum
 
     Private Sub frmPartidos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'carga los nombres de los equipos en los ComboBox
+        'carga los nombres de los equipos en los combobox
         cmbEquipLocal.Items.Add("Aldosivi")
         cmbEquipLocal.Items.Add("Argentinos")
         cmbEquipLocal.Items.Add("Arsenal")
@@ -96,6 +98,19 @@
         Dim EstadoActualFormulario As EstadoFormulario = CheckearEstadoFormulario()
 
         Select Case EstadoActualFormulario
+            Case EstadoFormulario.No_Selecciono_Equipo_Local
+                MessageBox.Show("No selecciono un equipo de local." _
+                , "Atencion", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            Case EstadoFormulario.No_Selecciono_Equipo_Visitante
+                MessageBox.Show("No selecciono un equipo de visitante." _
+                , "Atencion", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            Case EstadoFormulario.Equipos_Duplicados
+                MessageBox.Show("No puede seleccionar el mismo equipo en ambos casilleros." _
+                & " Por favor, seleccione equipos diferentes como local y visitante." _
+                , "Atencion", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
             Case EstadoFormulario.Goles_Local_Es_Vacio
                 MessageBox.Show("No ha ingresado un numero en los goles del equipo local." _
                 , "Atencion", MessageBoxButtons.OK, MessageBoxIcon.Information)
@@ -129,11 +144,6 @@
                 & " Por favor, seleccione una de las opciones." _
                 , "Atencion", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
-            Case EstadoFormulario.Equipos_Duplicados
-                MessageBox.Show("No puede seleccionar el mismo equipo en ambos casilleros." _
-                & " Por favor, seleccione equipos diferentes como local y visitante." _
-                , "Atencion", MessageBoxButtons.OK, MessageBoxIcon.Information)
-
             Case EstadoFormulario.Equipos_Enfrentados_Previamente
                 MessageBox.Show("Estos equipos ya se han enfrentado en otra ocasion. " _
                 & " Por favor, revise la lista de partidos jugados para mas detalles." _
@@ -149,7 +159,16 @@
         Dim golesLocal As Short
         Dim golesVisitante As Short
 
-        If txtGolesLocal.Text.Trim = String.Empty Then
+        If cmbEquipLocal.Text = String.Empty Then
+            Return EstadoFormulario.No_Selecciono_Equipo_Local
+
+        ElseIf cmbEquipVisitante.Text = String.Empty Then
+            Return EstadoFormulario.No_Selecciono_Equipo_Visitante
+
+        ElseIf cmbEquipLocal.Text = cmbEquipVisitante.Text Then
+            Return EstadoFormulario.Equipos_Duplicados
+
+        ElseIf txtGolesLocal.Text.Trim = String.Empty Then
             Return EstadoFormulario.Goles_Local_Es_Vacio
 
         ElseIf Not Short.TryParse(txtGolesLocal.Text.Trim, golesLocal) Then
@@ -167,22 +186,34 @@
         ElseIf golesVisitante < 0 Then
             Return EstadoFormulario.Goles_Visitante_Negativo
 
-        ElseIf False Then
-            'TODO crear una funcion SeHaSeleccionadoFinalizacion
+        ElseIf Not (rbNormal.Checked Or rbSuspClima.Checked Or rbSuspIncidentes.Checked) Then
             Return EstadoFormulario.No_Selecciono_Finalizacion
 
-        ElseIf False Then
-            'TODO crear una funcion que determine ambos combobox contienen el mismo equipo
-            Return EstadoFormulario.Equipos_Duplicados
-
-        ElseIf False Then
-            'TODO crear una funcion que determine, de los equipos que estan en la planilla,
-            'si ya se habian enfrentado previamente
+        ElseIf SeHanEnfrentadoAmbosEquipos() Then
             Return EstadoFormulario.Equipos_Enfrentados_Previamente
 
         Else
             Return EstadoFormulario.Correcto
         End If
+    End Function
+
+    Private Function SeHanEnfrentadoAmbosEquipos() As Boolean
+        Dim equipoLocal As String = cmbEquipLocal.Text
+        Dim equipoVisitante As String = cmbEquipVisitante.Text
+
+        For Each Partido In lsvPartidosJugados.Items
+            If equipoLocal = Partido.SubItems(1).Text Then
+                If equipoVisitante = Partido.SubItems(2).Text Then
+                    Return True
+                End If
+            ElseIf equipoVisitante = Partido.SubItems(1).Text Then
+                If equipoLocal = Partido.SubItems(2).Text Then
+                    Return True
+                End If
+            End If
+        Next
+
+        Return False
     End Function
 
     Private Sub CargarEjemplosLista()
